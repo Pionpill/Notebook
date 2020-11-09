dbbc1b1feef9c75f0c5c5f3d6890fa4a6533c514
Pionpill <673486387@qq.com>
2020-11-09 12:57:14 +0800

    no

---
+++ programming language/high-level language/Python/example_code/days.py
@@ -0,0 +1,3 @@
+import calendar
+monthRange = calendar.monthrange(2016, 9)
+print(monthRange)
---
+++ programming language/high-level language/Python/example_code/file IO.py
@@ -0,0 +1,8 @@
+with open("test.txt", "wt") as out_file:
+    out_file.write("该文本会写入到文件中\n看到我了吧！")
+
+# Read a file
+with open("test.txt", "rt") as in_file:
+    text = in_file.read()
+
+print(text)
