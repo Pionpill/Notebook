88054006db7d38bcc912c45734438a2469b8d884
Pionpill <673486387@qq.com>
2021-02-23 12:19:33 +0800

    1

---
+++ Language/Universal/JAVA/Test.java
@@ -0,0 +1,9 @@
+package Language.Universal.JAVA;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.printf("hello world!!\n");
+        System.out.print("hello world!!!");
+    }
+}
